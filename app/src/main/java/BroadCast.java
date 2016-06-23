a95253afb6f5401b9b4d20d7d93ae7cb019ac3f9
Filipoff <marto_1337@hotmail.com>
2016-06-23 18:47:34 +0300

    working infoget

---
+++ app/src/main/java/BroadCast.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Filipoff on 23.6.2016 г..
+ */
+public class BroadCast {
+    public String time;
+    public String telecast;
+}
--- app/src/main/java/net/unisofia/fmi/marto/vasko/tv_guide/BroadCast.java
+++ app/src/main/java/net/unisofia/fmi/marto/vasko/tv_guide/BroadCast.java
@@ -3,6 +3,7 @@ package net.unisofia.fmi.marto.vasko.tv_guide;
 /**
  * Created by Filipoff on 23.6.2016 г..
  */
-public interface CallBackListener {
-    public void callback();
+public class BroadCast {
+    public String time;
+    public String telecast;
 }
