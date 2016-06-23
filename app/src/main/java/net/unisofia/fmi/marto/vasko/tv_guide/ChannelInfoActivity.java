ae2a5455992441868c35939605b00a5451ed0212
Filipoff <marto_1337@hotmail.com>
2016-06-23 18:55:07 +0300

    empty commit

--- app/src/main/java/net/unisofia/fmi/marto/vasko/tv_guide/ChannelInfoActivity.java
+++ app/src/main/java/net/unisofia/fmi/marto/vasko/tv_guide/ChannelInfoActivity.java
@@ -1,15 +1,12 @@
 package net.unisofia.fmi.marto.vasko.tv_guide;
 
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,11 +15,60 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class ChannelInfoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private List<String> categories = new ArrayList<>();
+
+    private static final String BaseURL = "http://tv.dir.bg/tv_channel.php";
+    private String channelCode;
+
+    // only for today for now
+    private static final int offset = 0;
+
+
+    private String getDate(int offset){
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.DAY_OF_MONTH, offset);
+        Integer d = now.get(Calendar.DAY_OF_MONTH);
+        String day = d.toString();
+        Integer m = now.get(Calendar.MONTH) + 1;
+        String month = m.toString();
+        month = m < 10 ? "0" + month : month;
+        return day + "." + month;
+    }
+    private ArrayList<BroadCast> getSchedule(int offset){
+
+        ArrayList<BroadCast> schedule = new ArrayList<>();
+
+        String channelURL = BaseURL + "?id=" + channelCode + "&dd=" +getDate(offset);
+
+        try {
+            Document doc = Jsoup
+                    .connect(channelURL)
+                    .userAgent(
+                            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+                    .get();
+
+            Elements elements = doc.select("#events > li > a");
+
+
+            for (Element el : elements) {
+                BroadCast bc = new BroadCast();
+                bc.time = el.child(0).ownText();
+                bc.telecast = el.child(1).ownText();
+                schedule.add(bc);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return schedule;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +95,27 @@ public class ChannelInfoActivity extends AppCompatActivity implements AdapterVie
         spinner.setSelection(1);
 
 
+    }
+
+    private class ParseURL extends AsyncTask<String, Void, ArrayList<BroadCast>> {
+
+
+        @Override
+        protected ArrayList<BroadCast> doInBackground(String... strings) {
+
+            return getSchedule(offset);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<BroadCast> result) {
+
+            StringBuilder sb = new StringBuilder();
+            for (BroadCast bc : result){
+                sb.append(bc.time);
+                sb.append(" ");
+                sb.append(bc.telecast);
+            }
+        }
     }
 
     private void update(int position) {
